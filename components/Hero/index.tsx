6f13c5979d26c5d31d72fda6b34afa18948d81c6
Fileini <francescomaria.falini@gmail.com>
2026-04-26 22:18:39 +0200

    change buymecoffee slug

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -50,7 +50,7 @@ const Hero = () => {
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="https://buymeacoffee.com/francescomariafalini"
+                    href="https://buymeacoffee.com/storysizer"
                     className="inline-flex items-center gap-2 rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
                     <span className="inline-flex h-6 w-6 items-center justify-center overflow-hidden rounded-sm bg-[#ffdd00]">
